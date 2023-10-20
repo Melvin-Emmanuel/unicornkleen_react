10fc89a2e3d88ab852c32486d699f07402e9a952
Melvin-Emmanuel <emmanulmelv@gmail.com>
2023-10-20 16:51:58 +0100

    second commit

---
+++ src/Allroutes/Allroutes.tsx
@@ -0,0 +1,42 @@
+import { createBrowserRouter } from "react-router-dom";
+import react from "react"
+import Homepage from "../Homepage";
+import About from "../Pages/About";
+import Service from "../Pages/Service";
+import Contact from "../Pages/Contact";
+import Blog from "../Pages/Blog";
+
+const Index = createBrowserRouter([
+    {
+        path: "/",
+        element: <Homepage />,
+        children: [
+            {
+                index: true,
+               element:<Homepage/ >
+            }
+        ]
+    },
+    {
+        path: '/About',
+        element:<About/>
+    
+    }, {
+        path: '/Services',
+        element:<Service/>
+    },
+    {
+        path: "/Contact",
+        element:<Contact/>
+    },
+    {
+        path: "/Blog",
+        element:<Blog/>
+
+    }
+])
+
+
+
+export default Index
+
---
+++ src/Pages/Contact.tsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import styled from '@emotion/styled'
+
+const Container = styled.div`
+
+min-height: 100vh;
+width: 100%;
+`
+const Blog = styled.div`
+height: 300px;
+width: 100%;
+
+
+`
+
+
+const Contact = () => {
+  return (
+    <div>Contact</div>
+  )
+}
+
+export default Contact
